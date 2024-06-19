a18444a54b899b6e1dfb3dbfefa156f3dcf42b91
Thiseas C. Lamnidis <thisseass@gmail.com>
2024-06-19 11:00:04 +0200

    Do not delete KIN inputs

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 ## Clear directories with raw ENA data
-rm -rf /vol/volume/ena-data/
+rm -rf /vol/volume/ena-data/2024_Penske_NatureScientificReports/
 
 ## Clear directories with eager resources, but not results
 rm -rf /vol/volume/nf-core-eager/reference
 rm -rf /vol/volume/nf-core-eager/data
 
 ## Clear directories with KIN intermediate data
-rm -rf /vol/volume/genetic-kinship/practical/Schroeder_Poland_subset
+#rm -rf /vol/volume/genetic-kinship/practical/Schroeder_Poland_subset
 rm -rf /vol/volume/genetic-kinship/practical/KIN/splitbams/
