86215c89aaf9058d7820248842bb2e098294fcf8
Thiseas Christos Lamnidis <thisseass@gmail.com>
2025-05-27 12:49:43 +0200

    update script paths

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-## Clear directories with raw ENA data
-rm -rf /vol/volume/ena-data/2024_Penske_NatureScientificReports/
-
 ## Clear directories with eager resources, but not results
 rm -rf /vol/volume/nf-core-eager/reference
 rm -rf /vol/volume/nf-core-eager/data
