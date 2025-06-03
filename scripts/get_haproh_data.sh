5939527961b1da0c0fcd21f6a7be700a814133e0
Thiseas C. Lamnidis <thisseass@gmail.com>
2025-06-03 13:50:25 +0200

    Update get_haproh_data.sh

--- scripts/get_haproh_data.sh
+++ scripts/get_haproh_data.sh
@@ -12,6 +12,17 @@ git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull
 cp -r ~/summer_school_vms/data/day3_pkg/ /vol/volume/packages-poseidon/
 
 ## Then download the 1000G HDF5 files for for haproh.
-mkdir -p /vol/volume/haproh
+mkdir -p /vol/volume/haproh/data
 
 tar -xzf ~/summer_school_vms/data/haproh/1000g1240khdf5.tar.gz -C /vol/volume/haproh
+
+cd /vol/volume/haproh/data
+
+ena_ftp_links=(
+    "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR106/ERR10675925/HGC011.dslib_rmdup.q30.bam"
+    "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR106/ERR10675925/HGC011.dslib_rmdup.q30.bam.bai"
+    )
+
+for fn in ${ena_ftp_links[@]}; do
+    wget -L ${fn}
+done
