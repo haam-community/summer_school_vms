7d580889b0dc16fb60e7c256a441fa47975714b1
Thiseas C. Lamnidis <thisseass@gmail.com>
2025-06-11 12:12:41 +0200

    Update clear_space.sh

--- scripts/clear_space.sh
+++ scripts/clear_space.sh
@@ -3,7 +3,7 @@
 ## Clear directories with eager resources, but not results
 rm -rf /vol/volume/nf-core-eager/reference
 rm -rf /vol/volume/nf-core-eager/data/*fastq.gz
-rm -rf /vol/volume/nf-core-eager/analysis/
+rm -rf /vol/volume/nf-core-eager/analys{i,e}s/
 rm -rf /vol/volume/nf-core-eager/work/
 
 ## Clear directories with KIN intermediate data
