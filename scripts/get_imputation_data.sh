58feb2f388c0e537d98b00a7c067f2b80494c05b
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-06-01 15:38:44 +0200

    update script

--- scripts/get_imputation_data.sh
+++ scripts/get_imputation_data.sh
@@ -1,7 +1,11 @@
 ## Pull the summer school setup repo if not already done, then extract the zip of imputation files into the mounted volume.
 cd ~
 if [[ ! -d ~/summer_school_vms ]]; then
-    git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull
+    git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull --include "data/imputation/imputation.zip"
+else
+    cd ~/summer_school_vms &&
+    git lfs pull --include "data/imputation/imputation.zip"
+    cd ~
 fi
 
 unzip -d /vol/volume/imputation/ ~/summer_school_vms/data/imputation/imputation.zip
