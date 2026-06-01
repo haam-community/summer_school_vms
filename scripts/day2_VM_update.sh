6f1463602684a45da3b9152409767614954c2d05
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-06-01 15:55:21 +0200

    add script to download additional data and prepare environment

---
+++ scripts/day2_VM_update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd ~
+if [[ ! -d ~/summer_school_vms ]]; then
+    git clone https://github.com/haam-community/summer_school_vms.git
+else
+    cd ~/summer_school_vms
+    git pull
+    cd ~
+fi
+
+curl -L https://raw.githubusercontent.com/haam-community/summer_school_vms/refs/heads/main/scripts/get_qpwave_data.sh | bash
+
+## Add plink to the path for use in imputation (since it was not added to the environment, as it should have been).
+echo 'alias plink="/home/ubuntu/bin/miniconda3/envs/genetic-kinship/bin/plink"' >> ~/.bashrc
