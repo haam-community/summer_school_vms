a3cb26ac0136852e7eb3acd96ef96c064d2209ff
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-06-02 09:56:53 +0200

    add script to get unsupervised analysis data

---
+++ scripts/get_unsupervised_analysis_data.sh
@@ -0,0 +1,13 @@
+## Pull the summer school setup repo if not already done, then extract the zip of imputation files into the mounted volume.
+cd ~
+if [[ ! -d ~/summer_school_vms ]]; then
+    git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull --include "data/unsupervised_analyses/*.tar.gz"
+else
+    cd ~/summer_school_vms
+    git lfs pull --include "data/unsupervised_analyses/*.tar.gz"
+    cd ~
+fi
+
+mkdir -p /vol/volume/unsupervised_analyses
+yes y | tar -xzf ~/summer_school_vms/data/unsupervised_analyses/Data_ADMIX.tar.gz -C /vol/volume/unsupervised_analyses/
+yes y | tar -xzf ~/summer_school_vms/data/unsupervised_analyses/Data_MDS_PCA.tar.gz -C /vol/volume/unsupervised_analyses/
