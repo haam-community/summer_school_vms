2adb0e67eef00b56a083d7e64dc125fcbcc56329
Thiseas Christos Lamnidis <thisseass@gmail.com>
2026-06-02 10:12:01 +0200

    add waits

--- scripts/get_unsupervised_analysis_data.sh
+++ scripts/get_unsupervised_analysis_data.sh
@@ -1,10 +1,10 @@
 ## Pull the summer school setup repo if not already done, then extract the zip of imputation files into the mounted volume.
 cd ~
 if [[ ! -d ~/summer_school_vms ]]; then
-    git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull --include "data/unsupervised_analyses/*.tar.gz"
+    git clone https://github.com/haam-community/summer_school_vms.git & git lfs pull --include "data/unsupervised_analyses/*.tar.gz" & wait
 else
     cd ~/summer_school_vms
-    git lfs pull --include "data/unsupervised_analyses/*.tar.gz"
+    git lfs pull --include "data/unsupervised_analyses/*.tar.gz" & wait
     cd ~
 fi
 
